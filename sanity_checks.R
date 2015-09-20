fbbd134fc6072fdf4042b4196eaa3e55d0359e71
Rohan <rohanrao88@gmail.com>
2015-09-20 10:32:45 +0530

    combined basic steps

--- sanity_checks.R
+++ sanity_checks.R
@@ -1,6 +1,8 @@
 ### functions for basic data processing
 # 1. Removing redundant columns
-# 2. Removing duplicate columns
+# 2. Converting two-element columns to binary
+# 3. Removing duplicate columns
+# 4. Removing duplicate rows
 
 
 ## loading libraries
@@ -8,10 +10,9 @@ library(plyr)
 
 
 ## function for removing constant columns
-remove_redundant_columns <- function(X_train,X_test=data.frame())
+standard_cleaning <- function(X_train,X_test=data.frame())
 {
-	cat("Preparing Data\n")
-
+	# finding number of unique values in columns
 	ldf <- lapply(1:ncol(X_train), function(k)
 					{
 						return(data.frame("Column" = colnames(X_train)[k],
@@ -20,6 +21,7 @@ remove_redundant_columns <- function(X_train,X_test=data.frame())
 
 	ldf <- ldply(ldf, data.frame)
 
+	# removing redundant columns
 	if (min(ldf$Unique_Values == 1))
 	{
 		X_train <- X_train[,!names(X_train) %in% ldf$Column[ldf$Unique_Values == 1]]
@@ -33,23 +35,28 @@ remove_redundant_columns <- function(X_train,X_test=data.frame())
 		{
 			cat("Column", ldf$Column[ldf$Unique_Values == 1][i], "is removed from data\n")
 		}
-
-		return(list(X_train,X_test))
 	}else
 	{
 		cat("No redundant columns found\n")
-
-		return(list(X_train,X_test))
 	}
-}
 
+	# converting two-element columns to binary
+	if (nrow(ldf[ldf$Unique_Values == 2,]) > 0)
+	{
+		for (i in 1:nrow(ldf[ldf$Unique_Values == 2,]))
+		{
+			X_train[,i] <- as.numeric(as.factor(X_train[,i])) - 1
 
-## function for removing duplicate columns
-remove_duplicate_columns <- function(X_train,X_test=data.frame())
-{
-	cat("Preparing Data\n")
+			if (nrow(X_test) > 0)
+			{
+				X_test[,i] <- as.numeric(factor(X_test[,i], levels=X_train[,i])) - 1
+			}
 
-	X_train_new <- X_train[!duplicated(lapply(X_train,summary))]
+			cat("Column", ldf$Column[ldf$Unique_Values == 2][i], "is converted to binary\n")
+		}
+
+	# removing duplicate columns
+	X_train_new <- X_train[!duplicated(lapply(X_train,c))]
 
 	if (ncol(X_train_new) < ncol(X_train))
 	{
@@ -57,12 +64,20 @@ remove_duplicate_columns <- function(X_train,X_test=data.frame())
 
 		cat(ncol(X_train) - ncol(X_train_new), "duplicate columns removed from data\n")
 
-		return(list(X_train,X_test))
+		X_train <- X_train_new
 	}else
 	{
 		cat("No duplicate columns found\n")
-
-		return(list(X_train,X_test))
 	}
+
+	# removing duplicate rows
+	if (length(which(duplicated(X_train))) > 0)
+	{
+		X_train <- X_train[!duplicated(X_train),]
+
+		cat(length(which(duplicated(X_train))), "duplicate rows removed from data\n")
+	}
+
+	return(list(X_train,X_test))
 }
 
