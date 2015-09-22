6c08089d51fd924b909eb9d0d1cfaf9747b95269
Rohan <rohanrao88@gmail.com>
2015-09-22 16:09:35 +0530

    broken down into functions

--- processing_basic.R
+++ processing_basic.R
@@ -10,87 +10,85 @@ library(plyr)
 
 
 ## function for removing constant columns
-standard_cleaning <- function(X_train,X_test=data.frame())
+remove_redundant_columns <- function()
 {
-	# finding number of unique values in columns
-	ldf <- lapply(1:ncol(X_train), function(k)
-					{
-						return(data.frame("Column" = colnames(X_train)[k],
-										  "Unique_Values" = length(unique(X_train[,k]))))
-					})
+	count_unique <- lapply(X_train, function(k){length(unique(k))})
+	constant_columns <- names(count_unique[count_unique == 1])
 
-	ldf <- ldply(ldf, data.frame)
-
-	# removing redundant columns
-	if (min(ldf$Unique_Values == 1))
+	if (length(constant_columns) > 0)
 	{
-		X_train <- X_train[,!names(X_train) %in% ldf$Column[ldf$Unique_Values == 1]]
-		
-		if (nrow(X_test) > 0)
-		{
-			X_test <- X_test[,!names(X_test) %in% ldf$Column[ldf$Unique_Values == 1]]
-		}
+		X_train <<- X_train[,!names(X_train) %in% constant_columns]
+		X_test <<- X_test[,!names(X_test) %in% constant_columns]
 
-		for (i in 1:nrow(ldf[ldf$Unique_Values == 1,]))
-		{
-			cat("Column", ldf$Column[ldf$Unique_Values == 1][i], "is removed from data\n")
-		}
-	}else
+		cat(length(constant_columns), "redundant columns were removed from data\n")
+	}
+	
+	cat("\n")
+}
+
+
+## function for converting two-element columns to binary
+convert_binary_columns <- function()
+{
+	count_unique <- lapply(X_train, function(k){length(unique(k))})
+	binary_columns <- names(count_unique[count_unique == 2])
+
+	changes <- 0
+
+	for (i in which(colnames(X_train) %in% binary_columns))
 	{
-		cat("No redundant columns found\n")
+		if (all(unique(X_train[,i]) %in% c(0,1)) != T)
+		{
+			X_test[,i] <<- as.numeric(factor(X_test[,i], levels=X_train[,i])) - 1
+			X_test[is.na(X_test[,i]),i] <<- -1
+
+			X_train[,i] <<- as.numeric(as.factor(X_train[,i])) - 1
+
+			changes <- changes+1
+
+			cat("Column", colnames(X_train)[i], "converted to binary column\n")
+		}
 	}
 
-	cat("\n")
-
-	# converting two-element columns to binary
-	if (nrow(ldf[ldf$Unique_Values == 2,]) > 0)
-	{
-		for (i in 1:nrow(ldf[ldf$Unique_Values == 2,]))
-		{
-			X_train[,i] <- as.numeric(as.factor(X_train[,i])) - 1
-
-			if (nrow(X_test) > 0)
-			{
-				X_test[,i] <- as.numeric(factor(X_test[,i], levels=X_train[,i])) - 1
-			}
-
-			cat("Column", ldf$Column[ldf$Unique_Values == 2][i], "is converted to binary\n")
-		}
-	}else
+	if (changes == 0)
 	{
 		cat("No binary columns found\n")
 	}
 
 	cat("\n")
+}
 
-	# removing duplicate columns
-	X_train_new <- X_train[!duplicated(lapply(X_train,c))]
 
-	if (ncol(X_train_new) < ncol(X_train))
+## function for removing duplicate columns
+remove_duplicate_columns <- function()
+{
+	X_train <<- X_train[!duplicated(lapply(X_train,c))]
+
+	if (sum(duplicated(lapply(X_train,c))) > 0)
 	{
-		X_test <- X_test[,colnames(X_test) %in% colnames(X_train_new)]
+		X_test <<- X_test[,colnames(X_test) %in% colnames(X_train_new)]
 
-		cat(ncol(X_train) - ncol(X_train_new), "duplicate columns removed from data\n")
-
-		X_train <- X_train_new
+		cat(sum(duplicated(lapply(X_train,c))), "duplicate columns removed from data\n")
 	}else
 	{
 		cat("No duplicate columns found\n")
 	}
 
 	cat("\n")
+}
 
-	# removing duplicate rows
+
+## function for removing duplicate rows
+remove_duplicate_rows <- function()
+{
 	if (length(which(duplicated(X_train))) > 0)
 	{
-		X_train <- X_train[!duplicated(X_train),]
+		X_train <<- X_train[!duplicated(X_train),]
 
 		cat(length(which(duplicated(X_train))), "duplicate rows removed from data\n")
 	}else
 	{
 		cat("No duplicate rows found\n")
 	}
-
-	return(list(X_train,X_test))
 }
 
--- processing_categorical.R
+++ processing_categorical.R
@@ -32,15 +32,20 @@ onehot_encode_categories <- function(X_train,X_test=data.frame())
 		}
 	}
 
-	# creating dummy variables
-	cat("One-hot encoding the categorical variables")
-
+	# creating dummy features
 	if (length(categorical_columns) > 0)
 	{
+		cat("One-hot encoding the categorical features\n")
+
 		panel <- dummy.data.frame(panel, names=categorical_columns, sep="_")
 		colnames(panel) <- gsub("[[:punct:]]", "", colnames(panel))
 		colnames(panel) <- gsub("[[:space:]]+", " ", colnames(panel))
 		colnames(panel) <- gsub(" ", "_", colnames(panel))
+
+		cat("One-hot encoded", length(categorical_columns), "categorical features\n")
+	}else
+	{
+		cat("No categorical features found\n")
 	}
 
 	X_train <- panel[1:nrow(X_train),]
